a3b037168347cb25278489b8a3cd615514870ca3
m0slah <muhammedslahqadir@gmail.com>
2024-06-28 23:31:16 +0300

    First React Practice

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 // Spend a minute memorizing the line below
 // Re-write the line of code as best you can from memory
 
-ReactDOM.render(<p>Hello, React</p>, document.getElementById("root"));
+ReactDOM.render(
+  <ul>
+    <li>react</li>
+    <li>javascript</li>
+  </ul>,
+  document.getElementById("root")
+);
