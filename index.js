588c732df45544601033f7b4773d44c475bf08d0
m0slah <muhammedslahqadir@gmail.com>
2024-06-29 17:42:52 +0300

    createRoot API react

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
-ReactDOM.render(
-  <h1 className="header">Hello, React!</h1>,
-  document.getElementById("root")
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const navbar = (
+  <nav>
+    <h1>Bob's Bistro</h1>
+    <ul>
+      <li>Menu</li>
+      <li>About</li>
+      <li>Contact</li>
+    </ul>
+  </nav>
 );
 
-/* 
-Challenge - recreate the above line of code in vanilla JS by creating and
-appending an h1 to our div#root (without using innerHTML).
-
-- Create a new h1 element
-- Give it some textContent
-- Give it a class name of "header"
-- append it as a child of the div#root
-    
-*/
-
-// const h1 = document.createElement("h1");
-// h1.textContent = "hello from vanila javascript";
-// h1.className = "h1";
-// document.getElementById("root").append(h1);
+// ReactDOM.render(navbar, document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(navbar);
